5bdc253d4739f7ee10c388a20ceb44877eeec020
hefeng <hefeng02@agora.io>
2024-11-06 16:24:49 +0800

    init release/2.8.103

---
+++ publishAgoraCloudScene.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+## 定义项目根目录，确保脚本在正确的目录中运行
+#PROJECT_ROOT=$(dirname "$0")
+#
+## 进入项目根目录
+#cd "$PROJECT_ROOT" || exit
+
+./gradlew :AgoraCloudScene:clean
+
+# 执行 Gradle 命令并处理错误
+./gradlew :AgoraCloudScene:publishToMavenCentral --no-configuration-cache
+
+# 检查 Gradle 命令的退出状态码
+if [ $? -eq 0 ]; then
+  echo "Publish to Maven Central succeeded."
+else
+  echo "Publish to Maven Central failed."
+  exit 1
+fi
